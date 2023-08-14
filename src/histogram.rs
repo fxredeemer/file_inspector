ae45d1c5928206c3f5d704c2bf2ff6d76f603ba0
Feuerstein Flurin <ffeuerstein@gritec.ch>
2023-08-14 11:23:35 +0200

    generalized histogram creator

--- src/histogram.rs
+++ src/histogram.rs
@@ -1,9 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 pub struct HistogramCreator;
 
 impl HistogramCreator {
-    pub fn get_file_histogram(&self, bytes: Vec<u8>) -> HashMap<u8, i32> {
+    pub fn get_file_histogram<T>(&self, bytes: Vec<T>) -> HashMap<T, i32>
+    where
+        T: Eq + Hash,
+    {
         let mut values = HashMap::new();
 
         for byte in bytes {
@@ -20,7 +23,7 @@ mod test {
     use super::*;
 
     #[test]
-    pub fn get_file_histogram_by_default_returns_correct_histogram() {
+    pub fn get_file_histogram_numbers_returns_correct_histogram() {
         let vec = vec![1, 2, 3, 3, 3, 2, 4, 155];
         let creator = HistogramCreator;
         let histogram = creator.get_file_histogram(vec);
@@ -32,6 +35,22 @@ mod test {
         expected_histogram.insert(3, 3);
         expected_histogram.insert(4, 1);
         expected_histogram.insert(155, 1);
+
+        assert_eq!(histogram, expected_histogram)
+    }
+
+    #[test]
+    pub fn get_file_histogram_strings_returns_correct_histogram() {
+        let vec = vec!["🐃", "🐍", "🐄", "🐄", "🐍", "🌋", "🌋", "🐍"];
+        let creator = HistogramCreator;
+        let histogram = creator.get_file_histogram(vec);
+
+        let mut expected_histogram = HashMap::new();
+
+        expected_histogram.insert("🐍", 3);
+        expected_histogram.insert("🌋", 2);
+        expected_histogram.insert("🐄", 2);
+        expected_histogram.insert("🐃", 1);
 
         assert_eq!(histogram, expected_histogram)
     }
