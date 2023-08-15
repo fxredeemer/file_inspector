6509384b8507c3e27363c668b7186932f29ad978
Feuerstein Flurin <ffeuerstein@gritec.ch>
2023-08-15 13:42:20 +0200

    started plotter impl

--- src/plotter.rs
+++ src/plotter.rs
@@ -1,21 +1,35 @@
-use std::{collections::HashMap, error::Error};
-use terminal_size::{terminal_size, Height, Width};
+use anyhow;
+use std::collections::{HashMap, BTreeMap};
+use terminal_size::terminal_size;
 
 pub struct Plotter;
 
 impl Plotter {
-    pub fn plot_hiostogram(&self, counts: HashMap<u8, i32>) -> Result<(), Box<dyn Error>> {
-        let max_count = counts
+    pub fn plot_hiostogram(&self, counts: HashMap<u8, i32>) -> anyhow::Result<()> {
+        let (width, height) = terminal_size().ok_or(PlottingError::TerminalSizeNotDeterminable)?;
+        println!(
+            "The terminal is {} cols wide and {} lines tall",
+            width.0, height.0
+        );
+
+        if width.0 < 128 {
+            return Err(PlottingError::TerminalTooSmall.into());
+        }
+
+        let max_count: f64 = counts
             .iter()
             .map(|d| d.1)
             .max()
-            .ok_or(PlottingError::InvalidHistogram)?;
+            .ok_or(PlottingError::InvalidHistogram)?
+            .clone()
+            .into();
 
-        let (w, h) = terminal_size().ok_or(PlottingError::TerminalSizeNotDeterminable)?;
-        println!("The terminal is {} cols wide and {} lines tall", w.0, h.0);
+        let relative_counts: BTreeMap<u8, f64> = counts
+            .into_iter()
+            .map(|d| (d.0, (d.1 as f64 / max_count) * height.0 as f64))
+            .collect();
 
-        
-
+        println!("{:?}", relative_counts);
 
         Ok(())
     }
@@ -27,4 +41,6 @@ pub enum PlottingError {
     TerminalSizeNotDeterminable,
     #[error("Invalid histogram")]
     InvalidHistogram,
+    #[error("Terminal too small")]
+    TerminalTooSmall,
 }
